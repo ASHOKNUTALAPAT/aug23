d99e8f110ea430168f11ea0a67a130512074dff6
nutalapati.ashok <nutalapati.ashok@gmail.com>
2023-08-26 11:50:10 +0530

    version1.0

---
+++ session_app/src/main/java/FirstServlet.java
@@ -0,0 +1,45 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		//accessing request parameters
+		String num=request.getParameter("num1");
+		//creating HttpSession object(session creation)
+		HttpSession ht=request.getSession();
+		//setting session information
+		ht.setAttribute("session_id",num);
+		out.println("Session created...<br>");
+		out.println("<a href='SecondServlet'>CLICK HERE</a>");
+		out.close();
+	}
+
+}
---
+++ session_app/src/main/java/SecondServlet.java
@@ -0,0 +1,40 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		//session accessing
+		HttpSession ht1=request.getSession(false);
+		out.println("<br>Session Att Value:"+ht1.getAttribute("session_id"));
+		out.close();
+	}
+
+}
